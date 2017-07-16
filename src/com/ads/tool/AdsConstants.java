e9ebf3a23a02153bce65f719cf2c60c9aa67422f
1029278668@qq.com <1029278668@qq.com>
2017-07-16 22:46:17 +0800

    init
    init

---
+++ src/com/ads/tool/AdsConstants.java
@@ -0,0 +1,15 @@
+package com.ads.tool;
+
+public class AdsConstants {
+	// APP_ID 替换为你的应用从官方网站申请到的合法appId
+	public static final String APP_ID = "100034011";
+	public static final String SECRET_KEY = "d03b3b4ca941fb3812dd32690c947163";
+	public static final String BANNER1 = "61da97746b27de979aeaa5a45ebcdb24";
+	public static final String BANNER2 = "6182feeb546a19741af944c3b0aa61fe";
+	public static final String BANNER3 = "d4f6701e8d83eff6024f6fbcf8230390";
+	//public static final String BANNER4 = "b0bb8ceb32de7fa4721d0a1546f4b557";
+	public static final String INTERSTITIAL = "480dd46734d43d8506732e6d4d14a115";
+	public static final String APP_WALL = "ecdcfa2d48c5e9c0790bdc25ce7fcbcb";
+	public static final String MY_APP_WALL = "0598393896ac89dcdb3631140a44c0f3";
+
+}
